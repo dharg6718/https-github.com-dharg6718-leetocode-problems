6a1703554ff0fbfd992fcb6ab6e67275b5f7f2e7
Giridhar K R <143690086+dharg6718@users.noreply.github.com>
2026-07-10 09:54:18 +0530

    Time: 0 ms (100%), Space: 44.6 MB (69.1%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        
+        int i = n - 2;
+        
+        // Step 1: Find break point
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            int j = n - 1;
+            
+            // Step 2: Find just bigger element
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            
+            swap(nums, i, j);
+        }
+        
+        // Step 3: Reverse right side
+        reverse(nums, i + 1, n - 1);
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
